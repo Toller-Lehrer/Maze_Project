a7c43e6e83995df75568dfeb219e0687c0894017
Toller_Lehrer <moon06knight@gmail.com>
2024-12-31 12:53:24 +0330

    1234

--- test.cpp
+++ test.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 int main(){
     ofstream file("text0.txt");
-    for(int i = 0; i < 44 ; i++){
-        for(int j = 0; j < 66 ; j++){
+    for(int i = 0; i < 22 ; i++){
+        for(int j = 0; j < 63 ; j++){
           if (i == 0){
             if (j == 0) file << "|";
-            else if (j < 65) file << "_";
+            else if (j < 62) file << "_";
             else file << "|" << endl;
           }
-          else if ((i > 0) and (i < 44)){
+          else if ((i > 0) and (i < 22)){
             if (j == 0) file << "|";
-            else if (j == 65) file << " " << endl;
+            else if (j == 62) file << " " << endl;
             else if ((j % 2) == 0) file << "|";
             else file << "__";
           }
